33e25ca92908f575f74ceb7b937016ea378c6057
Your Name <you@example.com>
2017-03-11 19:22:47 +0100

    v.8

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animal = animal.toUpperCase();
+var text = text.replace('Papugi', animal);
+console.log(text.substr(0,text.length*0.5));
