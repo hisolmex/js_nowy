2a420dc85f33f2d98cd3ef04e162ce98f535988f
Your Name <you@example.com>
2017-03-11 19:20:54 +0100

    v.7

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
-// plik scripts.js
-function getTriangleArea(a, h) {
-    if ((a > 0) && (h > 0)) {
-        return a * h / 2;
-    } else {
-        return 'Nieprawidłowe dane';
-    };
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+console.log('Tablica zawiera nastêpuj¹ce imiona: ' + allNames);
+
+var newName = prompt('Podaj nowe imiê');
+
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+} else {
+  console.log('Podane imiê znajduje siê ju¿ w tablicy: ' + newName);
 }
 
-console.log(getTriangleArea(10, 6));
-
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(0, 8);
-var triangle3Area = getTriangleArea(14, 17);
+console.log('Ostatecznie tablica zawiera nastêpuj¹ce imiona: ' + allNames)
