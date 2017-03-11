2160cf3203188f3094b95de345b8ff90d6fd5494
Your Name <you@example.com>
2017-03-11 19:26:50 +0100

    v.10

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// plik scripts.js
+
+var buttons = document.getElementsByClassName('button');
+
+for (var i = 0; i < buttons.length; i++){
+  console.log(buttons[i].innerText)  
+}
+// onClick 
+function kliknij() {
+	alert('Przycisk 1');
+}
+function kliknij2() {
+	alert('Przycisk 2 ');
+}
+function kliknij3() {
+	alert('Przycisk 3 ');
+}
