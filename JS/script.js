35f2f446b383238ed50e44a1d11bcb6af65a3e30
Your Name <you@example.com>
2017-03-11 19:24:39 +0100

    v.8

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+function rysujChoinke(n) {
+    
+    for (var i = 1; i <= n; i++) {
+       var star =''; 
+    
+        for (var m = 1; m <= (i*2-1); m++) {
+             star += '*';
+        }
+        console.log(star);
+    }
+}
+
+rysujChoinke(10)
+
