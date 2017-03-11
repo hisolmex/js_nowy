e0fa3d227cff7dd9861aaf6ae87d882fedd825dc
Your Name <you@example.com>
2017-03-11 19:13:49 +0100

    v.3

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var name = prompt('Podaj swoje imię');
+alert('Witaj! ' + name);
+console.log('Witaj! ' + name);
+var a = prompt('Podaj długość podstawy trójkąta');
+var h = prompt('Podaj wysokość trójkąta');
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a= ' + a + ' i wysokości = ' + h + ' wynosi: ' + triangleArea);
