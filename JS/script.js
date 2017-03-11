38abbb23787c45a92c84d155999a918870da4f68
Your Name <you@example.com>
2017-03-11 19:09:40 +0100

    ver.2.0

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,3 @@
-// plik scripts.js
-function getTriangleArea(a, h) {
-    if ((a > 0) && (h > 0)) {
-        return a * h / 2;
-    } else {
-        return 'Nieprawidłowe dane';
-    };
-}
-
-console.log(getTriangleArea(10, 6));
-
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(0, 8);
-var triangle3Area = getTriangleArea(14, 17);
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
