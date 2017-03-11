0d12268e062dcd7010c386f80ef694d4103bb49a
Your Name <you@example.com>
2017-03-11 19:14:57 +0100

    v.4

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = prompt('Podaj wartość zmiennej (a)')
+var b = prompt('Podaj wartość zmiennej (b)')
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log(value);
+
+if (value > 0) {
+    console.log('Wynik dodatni');
+} else if (value < 0) {
+    console.log('Wynik ujemny');
+} else if (value === 0) {
+    console.log('Wynik jest równy 0');
+} else {
+    console.log('Wynik nie jest liczbą');
+};
